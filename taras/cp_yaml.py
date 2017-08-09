253143e76a34407966500aa0d6e93d54c8cf3b87
Taras Glek <taras@purestorage.com>
2017-08-09 15:56:34 -0700

    trying to move stuff to ignition

---
+++ taras/cp_yaml.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import sys
+"""
+cat bootkube-controller.yaml | ./cp_yaml.py --group etcd --user etcd `find assets/tls/etcd-* assets/tls/etcd` /etc/ssl/etcd/ | ./cp_yaml.py assets/auth/kubeconfig /etc/kubernetes/kubeconfig | ./cp_yaml.py --fullname `find assets/` /opt/bootkube/ |sed 's/^#/ /' > ../examples/ignition/bootkube-controller.yaml
+cat bootkube-worker.yaml | ./cp_yaml.py assets/auth/kubeconfig /etc/kubernetes/kubeconfig|sed 's/^#/ /' > ../examples/ignition/bootkube-worker.yaml
+"""
+import os
+import os.path
+import stat
+import argparse
+from ruamel import yaml
+
+def stat_mode(file_stat):
+  # can't return fancy 0700 type octals because python yaml outputs them as strings
+  return file_stat.st_mode & 0777
+  # return (oct(file_stat.st_mode & 0777))
+
+"""
+If dest ends with slash, means we are copying into a directory
+"""
+def maybe_join(dest, src, fullname):
+  if dest.endswith('/'):
+    leaf = src
+    if not fullname:
+      leaf = os.path.basename(leaf)
+    return os.path.join(dest, leaf)
+  else:
+    return dest
+
+def copy(src_files, dest_dir, opts):
+  y = yaml.round_trip_load(sys.stdin)
+  files = y['storage']['files']
+  try:
+    directories = y['storage']['directories']
+  except KeyError:
+    y['storage']['directories'] = directories = []
+
+  for p in files:
+    path = p['path']
+    if path in src_files:
+      raise KeyError("Refuse to overwrite file: " + path)
+  for filename in src_files:
+    file_stat = os.stat(filename)
+    entry = {
+        'path': maybe_join(dest_dir, filename, opts.fullname),
+        'filesystem': 'root',
+        'mode': stat_mode(file_stat),
+    }
+    if opts.user:
+        entry['user'] = dict([('name', opts.user)])
+    if opts.group:
+        entry['group'] = dict([('group', opts.user)])
+
+    if stat.S_ISDIR(file_stat.st_mode):
+      directories.append(entry)
+    else:
+      entry['contents'] = dict([('inline', open(filename, 'rb').read())])
+      files.append(entry)
+
+  # print 
+  yaml.round_trip_dump(y, sys.stdout)
+
+def main():
+  parser = argparse.ArgumentParser(description='Copy files into ignition yaml')
+  parser.add_argument('--user', dest='user', help='file owner user')
+  parser.add_argument('--group', dest='group', help='file owner group')
+  parser.add_argument('--fullname',  action='store_true', dest='fullname', default=False, help='trim src file to basename before appending to destdir')
+
+  (parsed, unparsed) = parser.parse_known_args()
+  copy(unparsed[:-1], unparsed[-1], parsed)
+
+main()
--- taras/part2.sh
+++ taras/part2.sh
@@ -3,26 +3,13 @@ set -x -e
 #bootkube render --asset-dir=assets --api-servers=https://node1.example.com:443 --api-server-alt-names=DNS=node1.example.com --etcd-servers=https://node1.example.com:2379
 #We're ready to use bootkube to create a temporary control plane and bootstrap a self-hosted Kubernetes cluster.
 
-#Secure copy the etcd TLS assets to /etc/ssl/etcd/* on every controller node.
-for node in 'node1'; do
-   scp -r assets/tls/etcd-* assets/tls/etcd core@$node.example.com:/home/core/
-   ssh core@$node.example.com 'sudo mkdir -p /etc/ssl/etcd && sudo mv etcd-* etcd /etc/ssl/etcd/ && sudo chown -R etcd:etcd /etc/ssl/etcd && sudo chmod -R 500 /etc/ssl/etcd/'
-done
-
-#Secure copy the kubeconfig to /etc/kubernetes/kubeconfig on every node to path activate the kubelet.service.
-for node in 'node1' 'node2' 'node3'; do
-    scp assets/auth/kubeconfig core@$node.example.com:/home/core/kubeconfig
-    ssh core@$node.example.com 'sudo mv kubeconfig /etc/kubernetes/kubeconfig'
-    ssh core@$node.example.com sudo systemctl enable iscsid
-    ssh core@$node.example.com sudo systemctl start iscsid
-done
-
 #echo give kubelet few min to start
 #sleep 120
 
 #Secure copy the bootkube generated assets to any controller node and run bootkube-start (takes ~10 minutes).
 
 scp -r assets core@node1.example.com:/home/core
+ssh core@node1.example.com 'sudo systemctl start bootkube'
 ssh core@node1.example.com 'sudo mv assets /opt/bootkube/assets && sudo systemctl start bootkube'
 
 #ssh core@node1.example.com journalctl -f
